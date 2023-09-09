cdbf612f33c5dfaeb59caa8f6d01a85e69cfd372
Marcuz Larsaen <t6b@topdanmark.dk>
2023-09-09 09:51:35 +0200

    feat: Monorepo for app complete

--- packages/app/index.js
+++ packages/app/index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import SkeletonExample from 'react-native-reanimated-skeleton/src/example';
 import { name as appName } from './app.json';
+
+/* NOTE: Require cycle is caused by worklets/index.ts and hooks/index.ts.
+It is not a problem so we ignore it. */
+LogBox.ignoreLogs(['Require cycle:']);
 
 AppRegistry.registerComponent(appName, () => SkeletonExample);
